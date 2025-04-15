37eeaa2367558b9a392685bafad7fe7e0b8a0c93
usernamewin <winonalvarez@gmail.com>
2025-04-15 13:48:36 +0800

    fire incident map

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('stations', views.map_station, name='map-station'),
-
+    path('incidents', views.map_incident, name='map-incident'),
 ]
