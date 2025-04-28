823226ecf26498c491a7410cdf6f5c81af414f5a
usernamewin <winonalvarez@gmail.com>
2025-04-28 17:06:33 +0800

    sucessfull chart setup

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -42,3 +42,13 @@ def map_incident(request):
         cities.add(incident.location.city)
 
     return render(request, 'map_incident.html', {'incidentData': incident_data, 'cities': list(cities)})
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
+from fire.views import HomePageView, ChartView
 from fire import views
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('stations', views.map_station, name='map-station'),
     path('incidents', views.map_incident, name='map-incident'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
 ]
