1b6baddb795c530f72221415a8fd8b071d8c267e
usernamewin <winonalvarez@gmail.com>
2025-04-28 17:21:00 +0800

    dynamic pie chart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView
+from fire.views import HomePageView, ChartView, PieCountbySeverity
 from fire import views
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('stations', views.map_station, name='map-station'),
     path('incidents', views.map_incident, name='map-incident'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
 ]
