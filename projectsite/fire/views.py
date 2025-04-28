08f3bb7dbe4be92e09f004d59a90e55628a55c49
usernamewin <winonalvarez@gmail.com>
2025-04-28 21:42:22 +0800

    modified chart.html and views.py

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -51,11 +51,11 @@ def map_incident(request):
 
 class ChartView(ListView):
     template_name = 'chart.html'
-
+ 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-
+     
     def get_queryset(self, *args, **kwargs):
         pass
 
